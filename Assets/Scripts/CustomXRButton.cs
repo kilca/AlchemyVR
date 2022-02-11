a6501a783a2a0907f278b413cacfa391f730ea4c
Kilian Cannet <kilian.cannet@u-psud.fr>
2022-02-11 17:39:55 +0100

    fix buttons

---
+++ Assets/Scripts/CustomXRButton.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CustomXRButton : XRBaseInteractable
+{
+
+}
